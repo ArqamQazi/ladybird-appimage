986547d274b314fcdbd05bc571eaffa41447de2a
ArqamQazi <arqamqazi549@gmail.com>
2026-09-18 21:50:32 +0530

    fix: restore /opt paths for Chaotic-AUR ladybird-git package and ANGLE libs

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,16 +9,34 @@ VERSION=${VERSION:-latest}
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
-export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+if [ -n "${GITHUB_REPOSITORY:-}" ]; then
+	export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+fi
 export ICON=https://raw.githubusercontent.com/LadybirdBrowser/ladybird/refs/heads/master/Base/res/icons/128x128/app-browser.png
 export DESKTOP=https://raw.githubusercontent.com/LadybirdBrowser/ladybird/refs/heads/master/Meta/CMake/freedesktop/org.ladybird.Ladybird.desktop
 export ANYLINUX_LIB=1
 
 # Deploy dependencies
-quick-sharun \
-	/usr/bin/[Ll]adybird* \
-	/usr/lib/ladybird \
-	/usr/share/ladybird
+if [ -d /opt/ladybird/usr ]; then
+	quick-sharun \
+		/opt/ladybird/usr/bin/*          \
+		/opt/ladybird/usr/lib/*          \
+		/opt/ladybird/usr/lib/ladybird/* \
+		/opt/angle/usr/lib/*             \
+		/opt/ladybird/usr/share/*
+else
+	quick-sharun \
+		/usr/bin/Ladybird                \
+		/usr/bin/js                      \
+		/usr/bin/wasm                    \
+		/usr/lib/ladybird/*              \
+		/usr/share/ladybird/*
+fi
+
+if [ -d ./AppDir/lib/angle/usr/lib ]; then
+	mv -v ./AppDir/lib/angle/usr/lib/* ./AppDir/lib/
+	rm -rf ./AppDir/lib/angle
+fi
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
